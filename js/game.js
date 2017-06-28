b3c686002bcb50d549564ebd68c5016ebf31b0d3
Benjamin Huang <ben.huang06@gmail.com>
2017-06-27 22:23:51 -0400

    added inversions

--- js/game.js
+++ js/game.js
@@ -1,55 +1,82 @@
+/**
+ * Block Slider
+ */
 
 (function(global) {
-    function Game(canvas, context) {
+    function Game(canvas, context, config) {
         // Game details
-        var sides = 3; // 3x3
+        var sides = 3, moves = 0; // 3x3... for now
+        var tiles = [], blank,
+            active = false;
 
-        var tiles = [], blank;
+        var cfg = {
+            winCallback: function() {
+                alert("you won!");
+            }
+        };
 
-        this.active = false;
+        if (config)
+            _extend(config, cfg);
 
         this.start = function() {
             if (!isSquare(canvas))
                 throw "Not a square.";
 
             if (canvas && context) {
-                this.active = true;
+                active = true;
                 context.fillStyle = "black";
                 context.fillRect(0, 0, canvas.width, canvas.height);
 
                 // Random blank tile pos
-                tiles = shuffle(fillTiles(tiles));
+                // And ensure combinations are solvable.
+                tiles = shuffle(fillTiles());
+
+                while (!solvable(tiles)) {
+                    shuffle(tiles);
+                }
+
+                console.log(tiles);
+
                 tiles.push({ order: 0, data: null });
                 blank = tiles.length - 1;
 
-                global.addEventListener("keydown", keyPush);
+                global.addEventListener("keydown", keyDown);
             }
         };
 
-        function keyPush(event) {
-            switch(event.keyCode) {
-                case 37:
-                    handleSwap(blank + 1);
-                    break;
-                case 38:
-                    handleSwap(blank + sides);
-                    break;
-                case 39:
-                    handleSwap(blank - 1);
-                    break;
-                case 40:
-                    handleSwap(blank - sides);
-                    break;
-            }
+        function keyDown(event) {
+            if (active) {
+                switch (event.keyCode) {
+                    case 37:
+                        handleSwap(blank + 1);
+                        break;
+                    case 38:
+                        handleSwap(blank + sides);
+                        break;
+                    case 39:
+                        handleSwap(blank - 1);
+                        break;
+                    case 40:
+                        handleSwap(blank - sides);
+                        break;
+                }
 
-            game();
-            //setTimeout(game(), 500);
+                moves++;
+                game();
+                //setTimeout(game(), 500);
+
+                // Check winner here
+                if (tilesInOrder(tiles)) {
+                    active = false;
+
+                    setTimeout(cfg.winCallback(), 200);
+                }
+            }
         }
 
         function handleSwap(blockIndex) {
             // Swappable?
             var difference = blank - blockIndex;
-            console.log(difference);
 
             // Right swap
             if (difference === -1 && (blockIndex) % sides === 0)
@@ -63,7 +90,6 @@
             if (blockIndex < 0 || blockIndex > tiles.length)
                 return;
 
-            console.log(tiles);
             console.log("Swapping indices " + blank + " and " + blockIndex);
             swapIn(tiles, blank, blockIndex);
             blank = blockIndex;
@@ -99,17 +125,25 @@
                 context.font = "48px roboto";
                 context.fillText(tiles[i].order, fillX + 50, fillY + 80);
                 fillX += blockWidth;
-
-                console.log("Filled in " + (c++));
             }
         }
 
         // TODO: check winner.
         function tilesInOrder(tiles) {
+            for (var i = 0; i < tiles.length - 1; i++) {
+                if (tiles[i].order === 0)
+                    return false;
 
+                if (tiles[i - 1] && tiles[i - 1].order > tiles[i].order)
+                    return false;
+            }
+
+            return true;
         }
 
-        function fillTiles(tiles) {
+        function fillTiles() {
+            var tiles = [];
+
             for (var i = 0; i < sides * sides - 1; i++) {
                 tiles.push({
                     order: i + 1,
@@ -149,7 +183,25 @@
         function isSquare(canvas) {
             return canvas.width === canvas.height;
         }
+
+        function solvable(tiles) {
+            var inversions = 0;
+
+            for (var i = 0; i < tiles.length; i++)
+                for (var j = i; j < tiles.length; j++)
+                    inversions += tiles[i].order > tiles[j].order;
+
+            return inversions % 2 === 0;
+        }
     }
 
-    global.blockGame = function(canvas, context) { return new Game(canvas, context); };
+    function _extend(source, host) {
+        for (var key in source) {
+            if (source.hasOwnProperty(key)) {
+                host[key] = source[key];
+            }
+        }
+    }
+
+    global.blockGame = function(canvas, context, config) { return new Game(canvas, context, config); };
 })(window);
